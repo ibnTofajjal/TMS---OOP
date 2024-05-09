bcedad1c589b76cd91a8d6cc98b14baebd22fb4f
ibnTofajjal <joynal.bintofajjal@gmail.com>
2024-05-10 01:25:45 +0600

    OOP_PROJECT: Complete School Management System.

--- School-Management-Sys/school.py
+++ School-Management-Sys/school.py
@@ -1,71 +1,134 @@
-class Shcool:
+class School:
     def __init__(self, name, address) -> None:
         self.name = name
         self.address = address
         self.teachers = {}
-        self.classroom = {}
+        # composition
+        self.classrooms = {}
+    
+    def add_classroom(self, classroom):
+        self.classrooms[classroom.name] = classroom
 
     def add_teacher(self, subject, teacher):
         self.teachers[subject] = teacher
-    
-    def add_classroom(self, classroom):
-        self.classroom[classroom.name] = classroom
 
-    def students(self):
-        pass
-
-    @staticmethod 
-    def calculate_grade(marks):
-        if marks >= 80 and marks <= 100:
-            return "A+"
-        elif marks >= 70 and marks < 80:
-            return "A"
-        elif marks >= 60 and marks < 70:
-            return "B"
-        elif marks >= 50 and marks < 60:
-            return "C"
-        elif marks >= 33 and marks < 50:
-            return "D"
+    def student_admission(self, student):
+        className = student.classroom.name
+        if className in self.classrooms:
+            self.classrooms[className].add_student(student)
         else:
-            return "F"
-        
-    @staticmethod
-    def grade_to_value(grade):
-        grade_map = {
-            'A+': 5.00,
-            'A': 4.00,
-            'A-': 3.50,
-            'B': 3.00,
-            'C': 2.00,
-            'D': 1.00,
-            'F': 0.00
-        }
+            print(f'No classroom as named{className}')
 
-        return grade_map[grade]
-    
     @staticmethod
-    def value_to_grade(value):
-        if value >=4.5 and value<=5.00:
+    def calculate_grade(marks):
+        if 80 <= marks <= 100:
             return 'A+'
-        elif value >= 3.5 and value <4.50:
+        elif 70 <= marks < 80:
             return 'A'
-        elif value >= 3.0 and value < 3.5:
+        elif 60 <= marks < 70:
             return 'A-'
-        elif value >= 2.5 and value < 3.0:
+        elif 50 <= marks < 60:
             return 'B'
-        elif value >= 2.0 and value < 2.5:
+        elif 40 <= marks < 50:
             return 'C'
-        elif value >= 1.0 and value < 2.0:
+        elif 33 <= marks < 40:
             return 'D'
         else:
             return 'F'
         
+    @staticmethod
+    def grade_to_value(grade):
+        grade_map = {
+            'A+': 5.00, 
+            'A': 4.00, 
+            'A-': 3.50, 
+            'B': 3.00, 
+            'C': 2.00, 
+            'D': 1.00, 
+            'F': 0.00
+            }
+        return grade_map[grade]
 
-        # Represents a classroom
+    @staticmethod
+    def value_to_grade(value):
+        if 4.5 <= value <= 5.00:
+            return 'A+'
+        elif 3.5 <= value < 4.5:
+            return 'A'
+        elif 3.0 <= value < 3.5:
+            return 'A-'
+        elif 2.5 <= value < 3.0:
+            return 'B'
+        elif 2.0 <= value < 2.5:
+            return 'C'
+        elif 1.0 <= value < 2.0:
+            return 'D'
+        else:
+            return 'F'
+
     def __repr__(self) -> str:
-        # All Classroom
-        # All Teachers
-        # All Students
-        # All Subjects
-        # All Students Results
+        print('--------ALL CLASSROOMS--------')
+        for key, value in self.classrooms.items():
+            print(key)
+
+        print('--------Students-------')
+        eight = self.classrooms['eight']
+        for student in eight.students:
+            print(student.name)
+        
+        print('------subjects------')
+        for subject in eight.subjects:
+            print(subject.name, subject.teacher.name)
+
+        print('----Student Exam Marks-------')
+        for student in eight.students:
+            for key, value in student.marks.items():
+                print(student.name, key, value, student.subject_grade[key])
+            print('----student end----')
+        return ''
+            
+
+class ClassRoom:
+    def __init__(self, name) -> None:
+        self.name = name
+        # composition
+        self.students = []
+        self.subjects = []
+    
+    def add_student(self, student):
+        serial_id = f'{self.name}-{len(self.students) + 1}'
+        student.id = serial_id
+        self.students.append(student)
+
+    def add_subject(self, subject):
+        self.subjects.append(subject)
+
+    def take_semester_final(self):
+        # take exam
+        for subject in self.subjects:
+            subject.exam(self.students)
+        
+        # calculate final grade
+        for student in self.students:
+            student.calculate_final_grade()
+
+    def __str__(self) -> str:
+        return f'{self.name}-{len(self.students)}'
+    
+    # TODO: sort students by grade
+    def get_top_students(self):
         pass
+
+
+class Subject:
+    def __init__(self, name, teacher) -> None:
+        self.name = name
+        self.teacher = teacher
+        self.max_marks = 100
+        self.pass_marks = 30
+    
+    def exam(self, students):
+        for student in students:
+            mark = self.teacher.evaluate_exam()
+            student.marks[self.name] = mark
+            student.subject_grade[self.name] = School.calculate_grade(mark)
