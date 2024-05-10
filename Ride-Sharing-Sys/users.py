04e7c163ee0142e6b9e512626316fc78f63f363c
ibnTofajjal <joynal.bintofajjal@gmail.com>
2024-05-10 23:19:52 +0600

    Chore: Rider Class

---
+++ Ride-Sharing-Sys/users.py
@@ -0,0 +1,42 @@
+from abc import ABC, abstractmethod
+
+class User(ABC):
+    def __init__(self, name, email, nid) -> None:
+        self.name = name
+        self.email = email
+        self.nid = nid
+        self.wallet = 0
+
+    @abstractmethod
+    def display_profile(self):
+        raise NotImplementedError
+    
+class Rider(User):
+    def __init__(self, name, email, nid, current_location, initial_ammount) -> None:
+        super().__init__(name, email, nid)
+        self.current_ride = None
+        self.current_location = current_location
+        self.wallet = initial_ammount
+
+    # Display Profile
+    def display_profile(self):
+        print(f"Rider Name: {self.name}, \n Email: {self.email}")
+
+    # Load to Wallet
+    def load_cash(self, amount):
+        if amount > 0:
+            self.wallet += amount
+        else:
+            print("Amount is Less than 0")
+
+    # Update Location
+    def update_location(self, current_location):
+        self.current_location = current_location
+
+    # Request for a Ride
+    def request_ride(self):
+        pass
+
+    # Show Current Ride
+    def show_current_ride(self):
+        print(self.current_ride)
